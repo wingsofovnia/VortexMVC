8c0a68ad3ceb05deaa7b0f29def79d18a4e52b1f
Rostyslav <the.nulpatrol@gmail.com>
2014-06-08 23:33:49 +0300

    Move some methods from Application to Router

--- lib/Vortex/Router.php
+++ lib/Vortex/Router.php
@@ -16,19 +16,85 @@ class Vortex_Router {
     private $params;
     private $url;
     private $routes;
-
+	private $config;
+	private $request;
+    private $response;
+	
     /**
      * Inits defaults
      */
     public function __construct() {
+	    $this->request = new Vortex_Request();
+        $this->response = new Vortex_Response();
         $this->controller = Vortex_Config::getInstance()->controller->default;
         $this->action = Vortex_Config::getInstance()->action->default;
         $this->params = array();
         $this->url = '';
         $this->routes = array();
     }
-
-    /**
+    
+	/**
+     * Run the application
+     * @throws Vortex_Exception_ControllerError if controller doesn't exists (and PRODUCTION state = 0)
+     */
+    public function run() {
+        ob_start();
+        $this->initConfigs();
+        $this->initRouter();
+        $this->request->addParams($this->getParams());
+        try {
+            $this->initAction($this->getController(), $this->getAction());
+        } catch (Vortex_Exception_InitError $e) {
+            try {
+                $this->initAction($this->config->controller->error, $this->config->action->error);
+            } catch (Vortex_Exception_InitError $e) {
+                throw $e;
+            }
+        }
+        $content = ob_get_clean();
+        $this->response->setBody($content);
+        $this->response->sendPacket();
+    }
+	
+	/**
+     * Includes application.php configs
+     */
+    private function initConfigs() {
+        $this->config = Vortex_Config::getInstance();
+    }
+    
+	/**
+     * Inits router configs and parsing URL
+     */
+    private function initRouter() {
+        $this->setUrl($_SERVER['REQUEST_URI']);
+        $routes = $this->config->routes;
+        foreach ($routes as $route) {
+            $this->registerRoute((array)$route);
+        }
+        $this->parse();
+    }
+	
+	/**
+     * Runs an action of controller
+     * @param string $controller name of controller
+     * @param string $action name of action
+     * @throws Vortex_Exception_InitError if controller or action doesn't exists
+     */
+    private function initAction($controller, $action) {
+        $controller .= 'Controller';
+        $controllerPath = APPLICATION_PATH . '/controllers/' . $controller . '.php';
+        if (!file_exists($controllerPath))
+            throw new Vortex_Exception_InitError('Controller does\'t exists!');
+        require_once $controllerPath;
+        $controller = new $controller($this->request, $this->response);
+        $action .= 'Action';
+        if (is_callable(array($controller, $action)) == false)
+            throw new Vortex_Exception_InitError('Action does\'t exists!');
+        $controller->$action();
+    }
+	
+	/**
      * Process the url.
      * This method doesn't actually parse url, but decide if it
      * should be parsed or predefined route could be used.
