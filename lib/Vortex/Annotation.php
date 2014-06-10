efe8fb2948a520fa688a05b12af911c2d66fda17
Rostyslav Khanyukov <the.nulpatrol@gmail.com>
2014-06-11 01:17:44 +0300

    * Added Annotation class

---
+++ lib/Vortex/Annotation.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Project: VortexMVC
+ * Author: Rostislav Khanyukov
+ * Date: 11-Jun-14
+ * Time: 1:03
+ */
+ 
+/**
+ * Class Vortex_Annotation
+ * This class implements annotations
+ */ 
+
+class Vortex_Annotation {
+	
+	private $pattern = '/\s*\*\s*\@([a-z0-9-_]+)\((.*)\).*/i';
+	
+	/**
+     * Gets data from doc-comment
+     * @param string $classname classname
+	 * @return parsed data from doc-comment
+     */
+	public function getAnnotations($classname) {
+		$classInfo = new ReflectionClass($classname);
+		$docComment = $classInfo->getDocComment();
+		if (preg_match_all($this->pattern, $docComment, $matches)) {
+			foreach ($matches[1] as $i => $key) {
+				$values = explode(',', trim($matches[2][$i]));
+				foreach ($values as $k => $v) {
+		    		$data[$key][] = trim($v, "' ");
+				}	 
+		    }
+		}
+		return $data;
+    }
+}
